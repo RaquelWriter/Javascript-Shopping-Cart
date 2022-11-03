c64297698ffead21dfec935edc8b50f0e05a6368
RaquelWriter <raquel@mundofeliz.com>
2022-11-03 14:23:35 +0100

    Adding IVA

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 5, 2022 12:00:00").getTime();
+var countDownDate = new Date("Nov 10, 2022 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
